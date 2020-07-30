f4cb5bb03a117850678b888932cfabea4a68edf7
Michael <mpun@uw.edu>
2020-07-30 12:07:45 -0700

    Add batch size considerations to network

--- tf2/hnn.py
+++ tf2/hnn.py
@@ -8,23 +8,33 @@ import nonlinearity
 
 class hnn(tf.keras.Model):
     
-    def __init__(self, L_MAX, hidden_l_dims, num_layers, cg_matrices, **kwargs):
+    def __init__(self, L_MAX, hidden_l_dims, num_layers, num_classes, cg_matrices, **kwargs):
         super().__init__(**kwargs)
         self.L_MAX = L_MAX
         self.hidden_l_dims = hidden_l_dims
         self.num_layers = num_layers
         self.cg_matrices = cg_matrices
-        
+        self.num_classes = num_classes
+
         temp_layers = []
         for i in range(num_layers):
             temp_layers.append(linearity.Linearity(hidden_l_dims[i], i, self.L_MAX))
             temp_layers.append(nonlinearity.Nonlinearity(self.L_MAX, self.cg_matrices))
+        temp_layers.append(tf.keras.layers.Dense(num_classes))
         self.layers_ = temp_layers
-            
+
+    @tf.function
     def call(self, input):
         curr_nodes = input
-        for layer in self.layers_:
+        for layer in self.layers_[:-1]:
             curr_nodes = layer(curr_nodes)
-        return curr_nodes
+        scalar_output = curr_nodes[0]
+        scalar_out_real = tf.math.real(scalar_output)
+        scalar_out_imag = tf.math.imag(scalar_output)
+        print('Scalar output: ')
+        print(scalar_output)
+        scalar_output = tf.squeeze(tf.concat([scalar_out_real,scalar_out_imag], axis=0))
+        output = self.layers_[-1](scalar_output)
+        return output
             
         
--- tf2/test.py
+++ tf2/test.py
@@ -4,32 +4,33 @@ import linearity
 import nonlinearity
 import hnn
 import os
-
-L_MAX = 1
-l_dims = [2,4]
-in_dims = [2,4]
-print('Declaring linearity layer')
-
-linear_layer = linearity.Linearity(l_dims, 0, L_MAX)
-
-linear_input = {}
-for l in range(L_MAX + 1):
-    linear_input[l] = tf.convert_to_tensor([tf.complex(np.random.random(2*l+1),
-                                                       np.random.random(2*l+1)) 
-                                           for i in range(in_dims[l])],dtype=tf.dtypes.complex64)
-
-print('Linear input:')
-print(linear_input)
-print('\n')
-
-linear_output = linear_layer(linear_input)
-
-print('Linear output:')
-print(linear_output)
-print('\n')
-
-
-
+# 
+#L_MAX = 1                      
+#l_dims = [2,4]
+#in_dims = [2,4]
+#print('Declaring linearity layer')
+#
+#linear_layer = linearity.Linearity(l_dims, 0, L_MAX)
+#
+#linear_input = {}
+#for l in range(L_MAX + 1):
+#    linear_input[l] = tf.convert_to_tensor([tf.complex(np.random.random(2*l+1),
+#                                                       np.random.random(2*l+1)) 
+#                                           for i in range(in_dims[l])],dtype=tf.dtypes.complex64)
+#
+#print('Linear input:')
+#print(linear_input)
+#print('\n')
+#
+#linear_output = linear_layer(linear_input)
+#
+#print('Linear output:')
+#print(linear_output)
+#print('\n')
+#
+#
+#
+L_MAX = 5
 cg_file = 'CG_matrix_l=5.npy'
 cg_matrices = np.load(cg_file, allow_pickle=True).item()
 
@@ -40,16 +41,16 @@ for l in range(L_MAX + 1):
             tf_cg_matrices[(l,l1,l2)] = tf.convert_to_tensor(cg_matrices[(l,l1,l2)],dtype=tf.complex64)
                     
 
-nonlinear_layer = nonlinearity.Nonlinearity(L_MAX, tf_cg_matrices)
-
-nonlinear_input = linear_output
-
-print('nonlinear output')
-print(nonlinear_layer)
-nonlinear_output = nonlinear_layer(nonlinear_input)
-
-print(nonlinear_output)
-
+#nonlinear_layer = nonlinearity.Nonlinearity(L_MAX, tf_cg_matrices)
+#
+#nonlinear_input = linear_output
+#
+#print('nonlinear output')
+#print(nonlinear_layer)
+#nonlinear_output = nonlinear_layer(nonlinear_input)
+#
+#print(nonlinear_output)
+#
 print('Testing hnn model')
 num_layers = 3
 L_MAX = 2
@@ -61,8 +62,25 @@ for i in range(num_layers):
     hidden_l_dims.append(curr_l_dims)
 print('Making network with L_MAX=' + str(L_MAX) + ' with '  + str(num_layers) + ' layers with hidden dimensions ' + 
       str(hidden_l_dims))
+num_aa = 20
+network = hnn.hnn(L_MAX, hidden_l_dims, num_layers, num_aa, tf_cg_matrices)
 
-network = hnn.hnn(L_MAX, hidden_l_dims, num_layers, tf_cg_matrices)
+print('Making test input to model')
+batch_size = 7
+in_dims = [3,2,1]
+network_input = {}
+for l in range(L_MAX + 1):
+    network_input[l] = tf.convert_to_tensor([[tf.complex(np.random.random(2*l+1),
+                                                       np.random.random(2*l+1)) 
+                                             for i in range(in_dims[l])]
+                                             for j in range(batch_size)]
+                                            ,dtype=tf.dtypes.complex64)
 
+
+print('network input:')
+print(network_input)
+    
+prediction = network(network_input)
+print(prediction)
 
 print('Terminating successfully')
