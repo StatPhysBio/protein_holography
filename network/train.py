ec3766b40e406b11488a5437b52b137bf2ce23db
Andrew Ivanov <andrew.ivanov@live.com>
2020-08-28 22:48:52 -0700

    hyak testing

--- network/train.py
+++ network/train.py
@@ -13,34 +13,66 @@ import clebsch
 from dataset import get_dataset
 import sys, os
 import logging
+from argparse import ArgumentParser
 
 logging.getLogger().setLevel(logging.INFO)
+default_datadir = os.path.join(os.path.dirname(__file__), "../data/")
+default_outputdir = os.path.join(os.path.dirname(__file__), "../output/")
+
+parser = ArgumentParser()
+parser.add_argument('-L',
+                    dest='L',
+                    type=int,
+                    default=6,
+                    help='L value')
+parser.add_argument('-k',
+                    dest='k',
+                    type=float,
+                    default=0.001,
+                    help='k value')
+parser.add_argument('-d',
+                    dest='d',
+                    type=float,
+                    default=10.0,
+                    help='d value')
+parser.add_argument('-e',
+                    dest='e',
+                    type=int,
+                    default=1000,
+                    help='examples per aminoacid')
+parser.add_argument('--datadir',
+                    dest='datadir',
+                    type=str,
+                    default=default_datadir,
+                    help='data directory')
+parser.add_argument('--outputdir',
+                    dest='outputdir',
+                    type=str,
+                    default=default_outputdir,
+                    help='data directory')
+
+args =  parser.parse_args()
 
 logging.info("GPUs Available: %d", len(tf.config.experimental.list_physical_devices('GPU')))
 tf.config.threading.set_intra_op_parallelism_threads(4)
 tf.config.threading.set_inter_op_parallelism_threads(1)
 
-L_MAX = 6
-# load premade holograms
-k = 0.001
-d = 10.0
-examples_per_aa = 1000
-examples_per_aa_val = 1000
-d_val = 10.0
+cg_file = os.path.join(args.datadir, "CG_matrix_l=10.npy")
+hologram_dir = os.path.join(args.datadir, "holograms")
 
-cg_file = '../CG_matrix_l=10.npy'
-hologram_dir = "../holograms"
-checkpoint_filepath = './saved_weights/weights'
+checkpoint_filepath = os.path.join(
+    args.outputdir,
+    "e={}_k={}_d={}_l={}/weights".format(args.e, args.k, args.d, args.L))
 
-tf_cg_matrices = clebsch.load_clebsch(cg_file, L_MAX)
+tf_cg_matrices = clebsch.load_clebsch(cg_file, args.L)
 
 # network parameters
 num_layers = 4
 num_aa = 20
-hidden_l_dims = [[10] * (L_MAX + 1)] * num_layers
-logging.info("L_MAX=%d, %d layers", L_MAX, num_layers)
+hidden_l_dims = [[10] * (args.L + 1)] * num_layers
+logging.info("L_MAX=%d, %d layers", args.L, num_layers)
 logging.info("Hidden dimensions: %s", hidden_l_dims)
-network = hnn.hnn(L_MAX, hidden_l_dims, num_layers, num_aa, tf_cg_matrices)
+network = hnn.hnn(args.L, hidden_l_dims, num_layers, num_aa, tf_cg_matrices)
 
 @tf.function
 def loss_fn(truth, pred):
@@ -52,8 +84,8 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
 network.compile(optimizer=optimizer, loss=loss_fn, metrics =['categorical_accuracy'])
 
-ds_train = get_dataset(hologram_dir, examples_per_aa, k, d, L_MAX)
-ds_val = get_dataset(hologram_dir, examples_per_aa_val, k, d, L_MAX)
+ds_train = get_dataset(hologram_dir, args.e, args.k, args.d, args.L)
+ds_val = get_dataset(hologram_dir, args.e, args.k, args.d, args.L)
 
 # training dataset shouldn't be truncated unless testing
 ds_train_trunc = ds_train.batch(2).take(50)
