b4e6b151270f3ef41397dfc5b91a699fbd5be557
michaelpun <mpun@uw.edu>
2021-04-22 13:07:26 -0700

    Add functionality for mmtf parsing

--- hologram/protein_dir_parse.py
+++ hologram/protein_dir_parse.py
@@ -5,10 +5,10 @@
 import os
 
 # get all protein names from the pdb files in a given directory
-def get_proteins_from_dir(protein_dir):
+def get_proteins_from_dir(protein_dir,suf='mmtf'):
 
     files = os.listdir(protein_dir)
-    proteins = [x[:-4] for x in files if 'pdb' in x[-3:]]
+    proteins = [x[:4] for x in files if suf in x[-1*len(suf):]]
 
     return proteins
 
