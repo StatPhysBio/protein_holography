15a8d4127bf60988a9a2ad3d29cf61a7325f5302
zacmon <zacander.mon@gmail.com>
2022-05-28 19:26:22 -0700

    Added some typing. Fixed os.path.join in case pdb_dir is not specified correctly.

--- protein_holography/coordinates/preprocessor_pdbs.py
+++ protein_holography/coordinates/preprocessor_pdbs.py
@@ -6,22 +6,33 @@ import os
 import signal
 import sys
 import time
+from typing import Callable, List
 import warnings
 sys.path.append('/gscratch/stf/mpun/software/PyRosetta4.Release.python38.linux.release-299')
 
-from Bio.PDB import PDBList, PDBParser
-from Bio.PDB.mmtf import MMTFParser
-from Bio.PDB.PDBExceptions import PDBConstructionWarning
 import h5py
 import numpy as np
-import pandas as pd
 import pyrosetta
-from pyrosetta.rosetta import core, protocols, numeric, basic, utility
 
 init_flags = '-ignore_unrecognized_res 1 -include_current -ex1 -ex2 -mute all -ignore_zero_occupancy false -obey_ENDMDL 1'
 pyrosetta.init(init_flags)
 
-def process_data(pdb,pdb_dir):
+def process_data(pdb: str, pdb_dir: str) -> np.ndarray:
+    """
+    Process data.
+
+    Parameters
+    ----------
+    pdb : str
+        Name of PDB file.
+    pdb_dir : str
+        Directory the PDB is in.
+
+    Returns
+    -------
+    numpy.ndarray
+        The processed pose.
+    """
     assert(process_data.callback)
 
     pdb = pdb.decode('utf-8')
@@ -32,43 +43,52 @@ def process_data(pdb,pdb_dir):
     #pdb_file = '/gscratch/scrubbed/mpun/data/CoV2_ACE2/' + pdb + '.pdb'
     #pdb_file = '/gscratch/stf/mpun/data/TCRStructure/pdbs/' + pdb + '.pdb'
     #pdb_file = '/gscratch/stf/mpun/data/proteinG/' + pdb + '.pdb'
-    pdb_file = pdb_dir + pdb + '.pdb'
-    
+    pdb_file = os.path.join(pdb_dir, pdb) + '.pdb'
     try:
         pose = pyrosetta.pose_from_pdb(pdb_file)
     except:
-        print('Pose could ot be created for protein {}'.format(pdb))
+        print(f'Pose could not be created for protein {pdb_file}.')
         return process_data.callback(None,**process_data.params)
     #print('pdb is ',pdb,pose.pdb_info().name())
     return process_data.callback(pose, **process_data.params)
 
-def initializer(init, callback, params, init_params):
+def initializer(init, callback: Callable, params, init_params):
     if init is not None:
         init(**init_params)
     process_data.callback = callback
     process_data.params = params
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-
 class PDBPreprocessor:
-    def __init__(self, hdf5_file, pdb_list, pdb_dir):
+    def __init__(self, hdf5_file: str, pdb_list: List[str], pdb_dir: str):
         with h5py.File(hdf5_file,'r') as f:
             pdb_list = np.array(f[pdb_list])
 
         self.pdb_dir = pdb_dir
         self.__data = pdb_list
         self.size = len(pdb_list)
-    def count(self):
+
+    def count(self) -> int:
+        """
+        Return the length of the data.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        int
+            Length of the data.
+        """
         return len(self.__data)
 
-    def execute(self, callback, parallelism = 8, limit = None, params = None, init = None, init_params = None):
+    def execute(self, callback: Callable, parallelism: int=8, limit: int=None, params = None, init = None, init_params = None) -> None:
         if limit is None:
             data = self.__data
         else:
             data = self.__data[:limit]
         with Pool(initializer = initializer, processes=parallelism, initargs = (init, callback, params, init_params)) as pool:
-
-    
             all_loaded = True
             if all_loaded:
                 # logging.info('All PDB files are loaded.')
