d873ae5876e86f4540381366adaafbc9172d0e87
michaelpun <mpun@uw.edu>
2022-10-25 15:07:40 -0700

    Update COA_ref_frame.py
    add docstrings. move squareroot to match name of magnitude

--- protein_holography/coordinates/COA_ref_frame.py
+++ protein_holography/coordinates/COA_ref_frame.py
@@ -1,15 +1,13 @@
-#
-# Python module for implementing creating the Carbon-Oxygen-alpha Carbon 
-# coordinate system 
-#
 import numpy as np
-# Levi-Civita tensor for taking cross product
+
 def get_eijk():
     """
     Constant Levi-Civita tensor
 
-    Returns:
-        tf.Tensor of shape [3, 3, 3]
+    Returns
+    -------
+    np.ndarray 
+        Levi-Civita tensor
     """
     eijk_ = np.zeros((3, 3, 3))
     eijk_[0, 1, 2] = eijk_[1, 2, 0] = eijk_[2, 0, 1] = 1.
@@ -17,8 +15,20 @@ def get_eijk():
     return eijk_
 
 def get_COA_axes(res):
+    """
+    COA axes as defined by the given residue
     
-    # check to make sure that all atoms necessary for the axes ar present
+    Parameters
+    ----------
+    res : Bio.PDB.Residue
+        
+    Returns
+    -------
+    np.ndarray
+        coordinate unit vectors
+    """
+    
+    # check to make sure that all atoms necessary for the axes are present
     if 'O' not in res:
         print('Error: No Oxygen found')
     if 'CA' not in res:
@@ -28,19 +38,31 @@ def get_COA_axes(res):
 
     # get coordinate system based on atom positions
     CCA_dir = res['C'].get_coord() - res['CA'].get_coord()
-    CCA_dir_mag = np.sum([x_*x_ for x_ in CCA_dir])
-    x_hat = (1./np.sqrt(CCA_dir_mag))*CCA_dir
+    CCA_dir_mag = np.sqrt(np.sum([x_*x_ for x_ in CCA_dir]))
+    x_hat = (1./CCA_dir_mag)*CCA_dir
     
     OCA_dir = res['O'].get_coord() - res['CA'].get_coord()
     y_dir = OCA_dir - np.einsum('i,i',OCA_dir,x_hat)*x_hat
-    y_dir_mag = np.sum([x_*x_ for x_ in y_dir])
-    y_hat = (1./np.sqrt(y_dir_mag))*y_dir
+    y_dir_mag = np.sqrt(np.sum([x_*x_ for x_ in y_dir]))
+    y_hat = (1./y_dir_mag)*y_dir
     
     z_hat = np.einsum('ijk,j,k->i',get_eijk(),x_hat,y_hat)
 
     return np.array([x_hat,y_hat,z_hat])
 
 def get_ACN_axes(res):
+    """
+    ACN axes as defined by the given residue
+    
+    Parameters
+    ----------
+    res : Bio.PDB.Residue
+        
+    Returns
+    -------
+    np.ndarray
+        coordinate unit vectors
+    """
     
     # check to make sure that all atoms necessary for the axes ar present
     if 'N' not in res:
@@ -52,13 +74,13 @@ def get_ACN_axes(res):
 
     # get coordinate system based on atom positions
     CCA_dir = res['CA'].get_coord() - res['C'].get_coord()
-    CCA_dir_mag = np.sum([x_*x_ for x_ in CCA_dir])
-    x_hat = (1./np.sqrt(CCA_dir_mag))*CCA_dir
+    CCA_dir_mag = np.sqrt(np.sum([x_*x_ for x_ in CCA_dir]))
+    x_hat = (1./CCA_dir_mag)*CCA_dir
     
     OCA_dir = res['CA'].get_coord() - res['N'].get_coord()
     y_dir = OCA_dir - np.einsum('i,i',OCA_dir,x_hat)*x_hat
-    y_dir_mag = np.sum([x_*x_ for x_ in y_dir])
-    y_hat = (1./np.sqrt(y_dir_mag))*y_dir
+    y_dir_mag = np.sqrt(np.sum([x_*x_ for x_ in y_dir]))
+    y_hat = (1./y_dir_mag)*y_dir
     
     z_hat = np.einsum('ijk,j,k->i',get_eijk(),x_hat,y_hat)
 
