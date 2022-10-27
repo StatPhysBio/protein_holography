9abec9aae8f29cc5c9c4bd4da485128e5f35a19d
michaelpun <mpun@n3279.hyak.local>
2022-10-27 14:49:52 -0700

    Add pytest for parallel processing of structures

---
+++ tests/test_parallel_coordinates.py
@@ -0,0 +1,70 @@
+"""Unit test for cooridnate gathering routine"""
+
+import os
+import sys
+import numpy as np
+
+import h5py
+#import pyrosetta
+import pytest
+
+
+from protein_holography.coordinates.get_structural_info import (
+    get_structural_info_from_dataset)
+from protein_holography.coordinates.pyrosetta_hdf5_neighborhoods import (
+    get_neighborhoods_from_protein,pad_neighborhoods)
+from protein_holography.coordinates.pyrosetta_hdf5_zernikegrams import get_hologram
+
+
+padded_length = 200000
+r_max = 10.
+L_max = 5
+ks = np.arange(21)
+num_combi_channels = 147
+
+
+    
+# init_flags = ('-ignore_unrecognized_res 1 -include_current -ex1 -ex2 -mute all'
+#               '-ignore_zero_occupancy false -obey_ENDMDL 1')
+# pyrosetta.init(init_flags)
+
+true_dataset = 'parallel_proteinG'
+with h5py.File('parallel_proteinG_true_proteins.hdf5','r') as f: 
+    true_structural_info = f[true_dataset][:]
+with h5py.File('parallel_proteinG_true_neighborhoods.hdf5','r') as f:
+    true_neighborhoods = f[true_dataset][:]
+with h5py.File('parallel_proteinG_true_zernikegrams.hdf5','r') as f:
+    true_zernikegrams = f[true_dataset][:]
+
+hdf5_in = 'parallel_proteinG_test_pdbs.hdf5'
+hdf5_out = 'parallel_proteinG_test_proteins.hdf5'
+pdb_list = 'parallel_proteinG'
+pdb_dir = '.'
+max_atoms = padded_length
+parallelism = 4
+get_structural_info_from_dataset(
+    hdf5_in, pdb_list, pdb_dir, max_atoms, hdf5_out, parallelism
+)
+
+         
+def test_structural_info():
+    with h5py.File(hdf5_out,'r') as f:
+        for i,test_structural_info in enumerate(f[pdb_list]):
+            assert test_structural_info == true_structural_info[i]
+
+            
+# def test_neighborhoods():
+
+#     assert (test_padded_neighborhoods == true_neighborhoods).all()
+
+# def test_zernikegrams():
+
+#     testing_zernikegrams = get_hologram(
+#         test_padded_neighborhoods[0],
+#         L_max,
+#         ks,
+#         num_combi_channels,
+#         r_max
+#     )
+
+#     assert (testing_zernikegrams == true_zernikegrams)
