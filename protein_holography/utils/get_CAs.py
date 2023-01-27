21b593b69d779093861b0183d05f2d9f6f4f964a
michaelpun <mpun@uw.edu>
2023-01-27 13:21:43 -0800

    Fix relative paths in squick run script and add get_CAs script for dynamic sizing of neighborhood hdf5

---
+++ protein_holography/utils/get_CAs.py
@@ -0,0 +1,20 @@
+import h5py
+import sys
+import numpy as np
+import sys
+
+def printname(x):
+    print(x)
+
+def main():
+
+    
+    with h5py.File(sys.argv[1],'r') as f:
+        total_cas = 0
+        for protein in f[sys.argv[2]]:
+            total_cas += np.count_nonzero(protein['atom_names'] == b' CA ')
+    sys.exit(str(total_cas))
+    
+
+if __name__ == "__main__":
+    main()
--- scripts/process_pdbs.sh
+++ scripts/process_pdbs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.bashrc
+source ~/source_conda.sh
 codedir="../protein_holography"
 datadir="../scripts"
 download_pdbs=1 # 1 to download pdb files. 0 to skip download
@@ -14,52 +16,55 @@ echo "Making necessary directories"
 download_tag=""
 if [ $download_pdbs -eq 1 ];
 then
-    echo "Downloading pdbs"
+    echo "Making pdb dir and downloading pdbs"
     mkdir $datadir/$project/pdbs
     download_tag="--download --pdb_dir $datadir/$project/pdbs"
+    echo -e "Pdbs downloaded\n"
 fi
+echo "Making data dirs for holographic steps"
 mkdir $datadir/$project/proteins
 mkdir $datadir/$project/neighborhoods
 mkdir $datadir/$project/zernikegrams
 mkdir $datadir/$project/energies
+echo -e "Directories creation attempted\n"
 
-echo "Making pdb file"
+echo "Making csv file of pdbs"
 python $codedir/coordinates/make_pdb_list.py --csv_file $csv_file \
        --hdf5_file $datadir/$project/${subproject}_pdbs.hdf5 \
        --dataset $subproject $download_tag 
 echo python $codedir/coordinates/make_pdb_list.py --csv_file $csv_file \
        --hdf5_file $datadir/$project/${subproject}_pdbs.hdf5 \
        --dataset $subproject $download_tag 
+echo -e "csv file made\n"
 
 echo "Gathering structural info from pdb files"
-python coordinates/get_structural_info.py --hdf5_out \
+python $codedir/coordinates/get_structural_info.py --hdf5_out \
        $datadir/$project/proteins/${subproject}_proteins.hdf5 \
        --pdb_list $subproject --parallelism $parallelism --hdf5_in \
        $datadir/$project/${subproject}_pdbs.hdf5\
        --pdb_dir $datadir/$project/pdbs/
-echo "Done gathering structural info"
+echo -e "Done gathering structural info\n"
 
 echo "Getting number of CAs"
-output=$((python /gscratch/spe/mpun/protein_holography/utils/get_CAs.py \
+output=$((python $codedir/utils/get_CAs.py \
 		 $datadir/${project}/proteins/${subproject}_proteins.hdf5 \
 		 $subproject) 2>&1)
-echo "Printing num CAs"
-echo "Found " $(($output)) " CAs in structural info"
+echo -e "Found " $(($output)) " CAs in structural info\n"
 
 echo "Gathering neighborhoods"
-python coordinates/get_neighborhoods.py --hdf5_out \
+python $codedir/coordinates/get_neighborhoods.py --hdf5_out \
        $datadir/$project/neighborhoods/${subproject}_neighborhoods.hdf5 \
        --hdf5_in $datadir/$project/proteins/${subproject}_proteins.hdf5 \
        --protein_list $subproject --parallelism $parallelism --num_nhs $output --r_max 10. 
-echo "Done gathering neighborhoods"
+echo -e "Done gathering neighborhoods\n"
 
 echo "Gathering zernikegrams"
-python coordinates/get_zernikegrams.py --hdf5_out \
+python $codedir/coordinates/get_zernikegrams.py --hdf5_out \
        $datadir/$project/zernikegrams/${subproject}_zernikegrams.hdf5 \
        --hdf5_in $datadir/$project/neighborhoods/${subproject}_neighborhoods.hdf5 \
        --neighborhood_list $subproject --parallelism $parallelism --num_nhs $output --Lmax 5 \
        -k 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 --r_max 10.
-echo "Done gathering zernikegrams"
+echo -e "Done gathering zernikegrams\n"
 
 
 echo "Making predictions"
@@ -68,4 +73,4 @@ python predict/predict.py --input zgram \
        --zgram_dataset $subproject --outfile $datadir/$project/energies/${subproject}_pseudoenergies.csv \
        --pnE_outfile $datadir/$project/energies/${subproject}_pnEs.csv \
        --network_dir $codedir/model_weights/best_network
-echo "Done making predictions"
+echo -e "Done making predictions\n"
