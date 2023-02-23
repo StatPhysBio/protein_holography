ffd11a4084095493a3b07bbda0b5c3ec38f7882b
michaelpun <mpun@uw.edu>
2023-02-23 14:33:20 -0800

    Allow for individual or batch testing via absolute paths dependent on package directory.
    Specifically use pathlib and package directory to find an absolute path to the
    data for each test so that tests can be run from any directory.

--- tests/parallel_test/test_parallel_coordinates.py
+++ tests/parallel_test/test_parallel_coordinates.py
@@ -3,13 +3,14 @@
 import os
 import sys
 import numpy as np
+from pathlib import Path
 
 import h5py
 #import pyrosetta
 import logging
 import pytest
 
-
+import protein_holography
 from protein_holography.coordinates.get_structural_info import (
     get_structural_info_from_dataset)
 from protein_holography.coordinates.get_neighborhoods import (
@@ -20,27 +21,45 @@ from protein_holography.coordinates.get_zernikegrams import (
 
 padded_length = 200000
 
+phdir = Path(protein_holography.__file__).parents[1]
     
 # init_flags = ('-ignore_unrecognized_res 1 -include_current -ex1 -ex2 -mute all'
 #               '-ignore_zero_occupancy false -obey_ENDMDL 1')
 # pyrosetta.init(init_flags)
 
 true_dataset = 'parallel_proteinG'
-with h5py.File('parallel_proteinG_true_proteins.hdf5', 'r') as f: 
+with h5py.File(
+    os.path.join(
+        phdir, 
+        "tests/parallel_test",
+        'parallel_proteinG_true_proteins.hdf5'),
+    'r') as f: 
     true_structural_info = f[true_dataset][:]
-with h5py.File('parallel_proteinG_true_neighborhoods.hdf5', 'r') as f:
+with h5py.File(
+    os.path.join(
+        phdir, 
+        "tests/parallel_test",
+        'parallel_proteinG_true_neighborhoods.hdf5'),
+    'r') as f:
     true_neighborhoods = f[true_dataset][:]
-with h5py.File('parallel_proteinG_true_zernikegrams.hdf5','r') as f:
+with h5py.File(
+    os.path.join(
+        phdir, 
+        "tests/parallel_test",
+        'parallel_proteinG_true_zernikegrams.hdf5'),
+    'r') as f:
     true_zernikegrams = f[true_dataset][:]
 
 
 #
 # structural info test
 #
-pdb_hdf5= 'parallel_proteinG_pdbs.hdf5'
-proteins_hdf5 = 'parallel_proteinG_test_proteins.hdf5'
+pdb_hdf5= os.path.join(
+    phdir, "tests/parallel_test", 'parallel_proteinG_pdbs.hdf5')
+proteins_hdf5 = os.path.join(
+    phdir, "tests/parallel_test", 'parallel_proteinG_test_proteins.hdf5')
 dataset = 'parallel_proteinG'
-pdbdir = '.'
+pdbdir = os.path.join(phdir, "tests/parallel_test")
 max_atoms = padded_length
 parallelism = 4
 get_structural_info_from_dataset(
@@ -51,6 +70,7 @@ def test_structural_info():
     with h5py.File(proteins_hdf5,'r') as f:
         for i,test_structural_info in enumerate(f[dataset]):
             assert test_structural_info == true_structural_info[i]
+    print(f"Size of protein file:{os.path.getsize(proteins_hdf5) / 1e6:.2f} MB")
     os.system(f"rm {proteins_hdf5}")
 #
 # neighborhoods test
@@ -70,6 +90,7 @@ def test_neighborhoods():
     with h5py.File(neighborhoods_hdf5,'r') as f:
         test_padded_neighborhoods = f[dataset][:]
         assert (test_padded_neighborhoods == true_neighborhoods).all()
+    print(f"Size of nh file:{os.path.getsize(neighborhoods_hdf5) / 1e6:.2f} MB")
     os.system(f"rm {neighborhoods_hdf5}")
         
 #
@@ -96,6 +117,7 @@ def test_zernikegrams():
     with h5py.File(zgram_hdf5,'r') as f:
         testing_zernikegrams = f[dataset][:]
     assert (testing_zernikegrams == true_zernikegrams).all()
+    print(f"Size of zgram file:{os.path.getsize(zgram_hdf5) / 1e6:.2f} MB")
     os.system(f"rm {zgram_hdf5}")
 
 
